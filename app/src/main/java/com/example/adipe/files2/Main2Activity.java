58d6dcd3ce32a16a09fdf80fd12dd0b1f6c607d5
adidi3 <ap0413@bs.amalnet.k12.il>
2019-01-20 13:16:09 +0200

    2nd

--- app/src/main/java/com/example/adipe/files2/Main2Activity.java
+++ app/src/main/java/com/example/adipe/files2/Main2Activity.java
@@ -5,6 +5,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -92,6 +94,29 @@ public class Main2Activity extends AppCompatActivity implements AdapterView.OnIt
             }
         c.close();
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menuCred) {
+            Toast.makeText(this, "the app created by Adi Eisenberg ", Toast.LENGTH_LONG).show();
+        }
+        if (id == R.id.mai) {
+            Intent t = new Intent(this, MainActivity.class);
+            startActivity(t);
+            finish();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
 
     @Override
